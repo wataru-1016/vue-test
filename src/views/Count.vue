d016e54368ccfb9899a0216deb4b807cbc86e82c
wataru yamazaki <wataru.y.1016@gmail.com>
2019-04-16 15:16:28 +0900

    vue-test

---
+++ src/views/Count.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="count">
+    <h1>This is an count page</h1>
+    {{$store.state.count}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // store.jsのmutasionsを呼ぶ場合はcommit、actionsを呼ぶ場合はdispatchで呼ぶ
+    increment() {
+      this.$store.commit("increment")
+    },
+    decrement() {
+      this.$store.commit("decrement")
+    }
+  }
+}
+</script>
---
+++ src/views/Other.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="other">
+    <h1>other</h1>
+  </div>
+</template>
